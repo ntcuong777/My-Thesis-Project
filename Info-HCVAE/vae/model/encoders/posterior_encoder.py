0402182c261f831ba71b3c9ee0b4f8de825c7ce7
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-12-13 01:47:43 +0700

    Revert "try gaussian za"
    This reverts commit 82fbf8da3c5edbf361a4b33cabb92e4aeab0c31a.

--- Info-HCVAE/vae/model/encoders/posterior_encoder.py
+++ Info-HCVAE/vae/model/encoders/posterior_encoder.py
@@ -29,8 +29,7 @@ class PosteriorEncoder(nn.Module):
         self.zq_attention = nn.Linear(nzqdim, 2 * nhidden)
 
         self.zq_linear = nn.Linear(4 * 2 * nhidden, 2 * nzqdim)
-        # self.za_linear = nn.Linear(nzqdim + 2 * 2 * nhidden, nza * nzadim)
-        self.za_linear = nn.Linear(nzqdim + 2 * 2 * nhidden, 2 * nzadim)
+        self.za_linear = nn.Linear(nzqdim + 2 * 2 * nhidden, nza * nzadim)
 
     def forward(self, c_ids, q_ids, a_ids):
         c_mask, c_lengths = return_mask_lengths(c_ids)
@@ -85,13 +84,11 @@ class PosteriorEncoder(nn.Module):
 
         h = torch.cat([zq, c_a_attned_by_zq, c_a_h], dim=-1)
 
-        # za_logits = self.za_linear(h).view(-1, self.nza, self.nzadim)
-        za_mu, za_logvar = torch.split(self.za_linear(h), self.nzadim, dim=1)
+        za_logits = self.za_linear(h).view(-1, self.nza, self.nzadim)
         # za_prob = F.softmax(za_logits, dim=-1)
-        za = sample_gaussian(za_mu, za_logvar)
+        za = gumbel_softmax(za_logits)
 
         if self.training:
-            # return zq_mu, zq_logvar, zq, za_logits, za
-            return zq_mu, zq_logvar, zq, za_mu, za_logvar, za
+            return zq_mu, zq_logvar, zq, za_logits, za
         else:
             return zq, za
