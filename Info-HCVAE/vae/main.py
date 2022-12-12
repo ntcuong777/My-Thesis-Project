65f5706b242779797915d01eecd0b8f336eaebc4
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-12-13 01:41:42 +0700

    Update main.py

--- Info-HCVAE/vae/main.py
+++ Info-HCVAE/vae/main.py
@@ -160,8 +160,8 @@ if __name__ == "__main__":
     parser.add_argument('--nzadim', type=int, default=20)
     parser.add_argument('--w_bce', type=float, default=1.0)
     parser.add_argument('--alpha_kl', type=float, default=0.0)
-    parser.add_argument('--lambda_mmd_q', type=float, default=1300.0)
-    parser.add_argument('--lambda_mmd_a', type=float, default=1300.0)
+    parser.add_argument('--lambda_mmd_q', type=float, default=7700.0)
+    parser.add_argument('--lambda_mmd_a', type=float, default=11700.0)
     # parser.add_argument('--alpha_jsd', type=float, default=0.0)
     parser.add_argument('--lambda_qa_info', type=float, default=1.0)
 
