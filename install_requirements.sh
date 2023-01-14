21a5c17f5b9781dcb5b665200fb8d42da459ebdb
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-14 08:43:18 +0700

    update install script

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,3 +1,3 @@
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
-pip install torch==1.12.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
+pip install torch==1.13.0+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install -r requirements.txt
