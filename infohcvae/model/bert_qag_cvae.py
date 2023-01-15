2077576b327f53278847ec9d63e659c766753293
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-15 22:23:02 +0700

    update loss weights

--- infohcvae/model/bert_qag_cvae.py
+++ infohcvae/model/bert_qag_cvae.py
@@ -195,8 +195,8 @@ class BertQAGConditionalVae(pl.LightningModule):
         parser.add_argument('--pooling_strategy', type=str, default="first", choices=["max", "mean", "first"])
         parser.add_argument('--alpha_kl_q', type=float, default=1)
         parser.add_argument('--alpha_kl_a', type=float, default=1)
-        parser.add_argument('--lambda_mmd_q', type=float, default=1)
-        parser.add_argument('--lambda_mmd_a', type=float, default=1)
+        parser.add_argument('--lambda_mmd_q', type=float, default=10)
+        parser.add_argument('--lambda_mmd_a', type=float, default=10)
         parser.add_argument('--lambda_qa_info', type=float, default=1)
 
         parser.add_argument("--lr", default=1e-3, type=float, help="lr")
