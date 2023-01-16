cef2d65a60c5c58dfd938d540822078157fb526c
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-16 09:55:35 +0700

    fix bug

--- infohcvae/model/bert_qag_cvae.py
+++ infohcvae/model/bert_qag_cvae.py
@@ -97,8 +97,7 @@ class BertQAGConditionalVae(pl.LightningModule):
         self.encoder = bert_model
 
         # Pooling layer for computing the latent variables
-        self.first_token_pooler = \
-            BertPooler(bert_model.get_encoder().config) if self.pooling_strategy == "first" else None
+        self.first_token_pooler = BertPooler(config) if self.pooling_strategy == "first" else None
 
         # Freeze embedding layer
         enc_embedding_layer = self.encoder.get_input_embeddings()
