abb665f85cdec098efc2d9a31701a097100216e3
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-17 15:47:49 +0700

    change run script

--- run_train.sh
+++ run_train.sh
@@ -1,2 +1,3 @@
 # remember to pass the batch size to this model as first arg, num_workers as 2nd flag
-python main.py --num_workers="$2" --accelerator=gpu --devices=auto --accumulate_grad_batches=8 --batch_size="$1" --save_frequency=5 --max_epochs=20 --log_every_n_steps=30 --check_val_every_n_epoch=5 --strategy=ddp --default_root_dir=./save/logs
+#--accumulate_grad_batches=8
+python main.py --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=5 --max_epochs=20 --log_every_n_steps=30 --check_val_every_n_epoch=5 --strategy=ddp --default_root_dir=./save/logs
--- run_train_load_saved_data.sh
+++ run_train_load_saved_data.sh
@@ -1,2 +1,3 @@
 # remember to pass the batch size to this model as first arg
-python main.py --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --accumulate_grad_batches=8 --batch_size="$1" --save_frequency=5 --max_epochs=20 --log_every_n_steps=30 --check_val_every_n_epoch=5 --strategy=ddp --default_root_dir=./save/logs
+# --accumulate_grad_batches=8
+python main.py --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=5 --max_epochs=20 --log_every_n_steps=30 --check_val_every_n_epoch=5 --strategy=ddp --default_root_dir=./save/logs
