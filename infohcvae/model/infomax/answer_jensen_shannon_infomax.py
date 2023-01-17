b0a7107c41771c46080805d71aa122a66368a15e
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-17 21:45:31 +0700

    fix

--- infohcvae/model/infomax/answer_jensen_shannon_infomax.py
+++ infohcvae/model/infomax/answer_jensen_shannon_infomax.py
@@ -16,10 +16,13 @@ class AnswerJensenShannonInfoMax(nn.Module):
 
     def forward(self, hidden_states, answer_mask, context_mask, answer_context_weight=0.2):
         answer_mask = answer_mask.type(torch.FloatTensor).to(hidden_states.device)
-        context_mask = context_mask.type(torch.FloatTensor)
+        context_mask = context_mask.type(torch.FloatTensor).to(hidden_states.device)
+
         answer_mask_mat = torch.matmul(answer_mask.unsqueeze(1), answer_mask.unsqueeze(2))
         answer_context_mask = torch.matmul(answer_mask.unsqueeze(1), context_mask.unsqueeze(2))
+
         pairwise_loss_info = self.infomax_loss(hidden_states, hidden_states)
+
         loss_answer_info = (pairwise_loss_info * answer_mask_mat).div(answer_mask_mat.sum())
         loss_answer_context_info = (pairwise_loss_info * answer_context_mask).div(answer_context_mask.sum())
         return (1. - answer_context_weight) * loss_answer_info + answer_context_weight * loss_answer_context_info
