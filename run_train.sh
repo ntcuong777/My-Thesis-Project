2c8e58fcd22ce681575a282a9694b7a3c77eeea9
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-19 03:13:24 +0700

    real run

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 # remember to pass the batch size to this model as first arg, num_workers as 2nd flag
 #--accumulate_grad_batches=8
-python main.py --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=1 --max_epochs=20 --log_every_n_steps=30 --check_val_every_n_epoch=2 --strategy=ddp --default_root_dir=./save/logs
+python main.py --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=2 --check_val_every_n_epoch=4 --max_epochs=20 --log_every_n_steps=30 --strategy=ddp --default_root_dir=./save/logs
