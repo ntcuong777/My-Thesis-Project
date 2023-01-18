c9160158d892e508e7a859147f92caeff2b22fa5
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-18 08:42:14 +0700

    fix

--- infohcvae/model/custom/gated_self_attention.py
+++ infohcvae/model/custom/gated_self_attention.py
@@ -7,8 +7,8 @@ class GatedAttention(nn.Module):
     def __init__(self, hidden_size=768):
         super(GatedAttention, self).__init__()
         self.context_attention = LuongAttention(hidden_size, hidden_size)
-        self.fusion = nn.Linear(2 * hidden_size, 2 * hidden_size, bias=False)
-        self.gate = nn.Linear(4 * hidden_size, 2 * hidden_size, bias=False)
+        self.fusion = nn.Linear(2 * hidden_size, hidden_size, bias=False)
+        self.gate = nn.Linear(2 * hidden_size, hidden_size, bias=False)
 
     def forward(self, hidden_states, attention_mask):
         mask = torch.matmul(attention_mask.unsqueeze(2), attention_mask.unsqueeze(1))
