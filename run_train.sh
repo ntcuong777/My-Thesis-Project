c9c3bf79bb2d26f4ad8d979b1334be57695060af
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-19 16:33:54 +0700

    not using any training strategy

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 # remember to pass the batch size to this model as first arg, num_workers as 2nd flag
 #--accumulate_grad_batches=8
-python main.py --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=4 --eval_frequency=4 --max_epochs=20 --log_every_n_steps=30 --strategy=ddp --default_root_dir=./save/logs
+python main.py --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=4 --eval_frequency=4 --max_epochs=20 --log_every_n_steps=30 --default_root_dir=./save/logs
