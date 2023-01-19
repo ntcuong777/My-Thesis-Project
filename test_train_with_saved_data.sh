aeee413ed8ebc963f2f169bfd704077e1ec9f5b7
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-19 12:32:02 +0700

    update eval code

--- test_train_with_saved_data.sh
+++ test_train_with_saved_data.sh
@@ -1,3 +1,3 @@
 # remember to pass the batch size to this model as first arg
 # --accumulate_grad_batches=8
-python main.py --limit_train_batches=0.03 --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=20 --eval_frequency=5 --max_epochs=20 --log_every_n_steps=30 --strategy=ddp --default_root_dir=./save/logs
+python main.py --limit_train_batches=0.03 --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=20 --eval_frequency=1 --max_epochs=20 --log_every_n_steps=30 --strategy=ddp --default_root_dir=./save/logs
