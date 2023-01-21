26e303cce7f38a8f3bf58bc88089a4cfb2660cbf
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-21 09:27:59 +0700

    update run script

--- run_train_load_saved_data.sh
+++ run_train_load_saved_data.sh
@@ -1,3 +1,3 @@
 # remember to pass the batch size to this model as first arg
 # --accumulate_grad_batches=8
-python main.py --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=4 --eval_frequency=4 --max_epochs=30 --log_every_n_steps=30 --default_root_dir=./save/logs
+python main.py --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=5 --eval_frequency=5 --max_epochs=30 --log_every_n_steps=30 --default_root_dir=./save/logs
