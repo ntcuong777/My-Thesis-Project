c67f143554e96c9477832ffbbf6b1f4261cf2324
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-23 23:39:01 +0700

    change run scripts

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,4 @@
 # remember to pass the batch size to this model as first arg, num_workers as 2nd flag
 #--accumulate_grad_batches=8
-python main.py --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=4 --eval_frequency=4 --max_epochs=20 --log_every_n_steps=30 --default_root_dir=./save/logs
+accum=$((64 / $1))
+python main.py --accumulate_grad_batches="$accum" --num_workers="$2" --accelerator=gpu --devices=auto --batch_size="$1" --save_frequency=4 --eval_frequency=4 --max_epochs=20 --log_every_n_steps=30 --default_root_dir=./save/logs
