e0f9febf7053fa591230aa905ea0e013544c940d
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-27 21:56:14 +0700

    update run scripts

--- run_train_load_saved_data.sh
+++ run_train_load_saved_data.sh
@@ -3,4 +3,4 @@
 tmp_1=64
 tmp_2=$1
 accum=$((tmp_1 / tmp_2))
-python main.py --accumulate_grad_batches="$accum" --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=5 --eval_frequency=5 --max_epochs=40 --log_every_n_steps=30 --default_root_dir=./save/logs
+python main.py --accumulate_grad_batches="$accum" --num_workers="$2" --accelerator=gpu --devices=auto --load_saved_dataloader --batch_size="$1" --save_frequency=5 --eval_frequency=5 --max_epochs=40 --log_every_n_steps=30 --check_val_every_n_epoch=40 --default_root_dir=./save/logs
