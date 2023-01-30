3774199dbb166bf4f538e48d6f473bce0affb835
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-01-31 02:24:01 +0700

    adjust answer mmd

--- infohcvae/model/bert_qag_cvae.py
+++ infohcvae/model/bert_qag_cvae.py
@@ -149,7 +149,7 @@ class BertQAGConditionalVae(pl.LightningModule):
         parser.add_argument("--alpha_kl_q", type=float, default=1)
         parser.add_argument("--alpha_kl_a", type=float, default=1)
         parser.add_argument("--lambda_mmd_q", type=float, default=1000)
-        parser.add_argument("--lambda_mmd_a", type=float, default=25000)
+        parser.add_argument("--lambda_mmd_a", type=float, default=10000)
         parser.add_argument("--lambda_qa_info", type=float, default=1)
 
         parser.add_argument("--lr", default=1e-3, type=float, help="lr")
