92448fd3137e727ead9d99d946ee7fd5057ec39b
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-02-08 01:04:09 +0700

    change lr and disc+gen optim parameters

--- infohcvae/model/bert_qag_cvae.py
+++ infohcvae/model/bert_qag_cvae.py
@@ -408,22 +408,22 @@ class BertQAGConditionalVae(pl.LightningModule):
         params_gen = filter(lambda p: p.requires_grad, params_gen)
 
         # 1st optimizer is optimizer for AE, 2nd is for discriminator
-        disc_lr = 1e-4
-        gen_lr = 3e-4
+        disc_lr = 2e-4
+        gen_lr = 2e-4
         if self.optimizer_algorithm == "sgd":
             optimizers = [optim.SGD(params_ae, lr=self.lr, momentum=0.9, nesterov=False),
                           optim.SGD(params_disc, lr=disc_lr, momentum=0.9, nesterov=False),
                           optim.SGD(params_gen, lr=gen_lr, momentum=0.9, nesterov=False)]
         elif self.optimizer_algorithm == "adam":
             optimizers = [optim.Adam(params_ae, lr=self.lr),
-                          optim.Adam(params_disc, lr=disc_lr),
-                          optim.Adam(params_gen, lr=gen_lr)]
+                          optim.Adam(params_disc, lr=disc_lr, betas=(0.5, 0.999)),
+                          optim.Adam(params_gen, lr=gen_lr, betas=(0.5, 0.999))]
         elif self.optimizer_algorithm == "adamw":
             optimizers = [optim.AdamW(params_ae, lr=self.lr),
-                          optim.AdamW(params_disc, lr=disc_lr),
-                          optim.AdamW(params_gen, lr=gen_lr)]
+                          optim.AdamW(params_disc, lr=disc_lr, betas=(0.5, 0.999)),
+                          optim.AdamW(params_gen, lr=gen_lr, betas=(0.5, 0.999))]
         else:
             optimizers = [additional_optim.SWATS(params_ae, lr=self.lr, nesterov=False),
-                          additional_optim.SWATS(params_disc, lr=disc_lr, nesterov=False),
-                          additional_optim.SWATS(params_gen, lr=gen_lr, nesterov=False)]
+                          additional_optim.SWATS(params_disc, lr=disc_lr, nesterov=False, betas=(0.5, 0.999)),
+                          additional_optim.SWATS(params_gen, lr=gen_lr, nesterov=False, betas=(0.5, 0.999))]
         return optimizers, []
